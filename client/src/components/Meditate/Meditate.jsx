367a81123240ecbac67d090675c16eefd0aac7a7
DeepaAnjanamurthy <76414473+DeepaAnjanamurthy@users.noreply.github.com>
2021-03-24 01:33:26 -0700

    added animation to card in meditate component

--- client/src/components/Meditate/Meditate.jsx
+++ client/src/components/Meditate/Meditate.jsx
@@ -33,64 +33,67 @@ getChakraInfo = () => {
 render(){
     // console.log(this.state.content);
     return (
-          <div>
-              <div className="pagetitle">
-                  <Link to={"/welcome"}>
-                      <img className="backicon" src={backIcon} alt="back icon"/>
-                  </Link>
-                  <h2 className="pageheading">Meditate</h2>
-              </div>
-              <div className="container">
-                  <div className="chakracard">
-                      <h2 className="chakraheading">{this.state.content.name}</h2>
-                      <img className="chakraimg" src={crownImg} alt="charka img"/>
-                      <div className="chakrainfo">
-                          <div>
-                              <h2 className="chakralabels">Sanskrit Name</h2>
-                              <p className="chakrapara">{this.state.content.sanskritname}</p>
-                          </div>
-                          <div>
-                              <h2 className="chakralabels">Element</h2>
-                              <p className="chakrapara">{this.state.content.element}</p>
-                          </div>
-                          <div>
-                              <h2 className="chakralabels">Function</h2>
-                              <p className="chakrapara">{this.state.content.function}</p>
-                          </div>
-                          <div>
-                              <h2 className="chakralabels">Related Body Parts</h2>
-                              <p className="chakrapara">{this.state.content.bodyparts}</p>
-                          </div>
-                          <div>
-                              <h2 className="chakralabels">Malfunction</h2>
-                              <p className="chakrapara">{this.state.content.malfunction}</p>
-                          </div>
-                      </div>
-                  </div>
-                  <div className="options">
-                      <Link className="linkchakra" to={`/meditatechakra/${this.props.match.params.id}/WMJ8ZyQuOxQ`}>
+        <div>
+            <div className="pagetitle">
+                <Link to={"/welcome"}>
+                    <img className="backicon" src={backIcon} alt="back icon"/>
+                </Link>
+                <h2 className="pageheading">Meditate</h2>
+            </div>
+            <div className="container">
+                <div className="chakracard card">
+                    <div className="card-side front">
+                        <h2 className="chakraheading">{this.state.content.name}</h2>
+                        <img className="chakraimg" src={crownImg} alt="charka img"/>
+                        <p className="clickformore">click here to learn more</p>
+                    </div>
+                    <div className="chakrainfo card-side back">
+                        <div>
+                            <h2 className="chakralabels">Sanskrit Name</h2>
+                            <p className="chakrapara">{this.state.content.sanskritname}</p>
+                        </div>
+                        <div>
+                            <h2 className="chakralabels">Element</h2>
+                            <p className="chakrapara">{this.state.content.element}</p>
+                        </div>
+                        <div>
+                            <h2 className="chakralabels">Function</h2>
+                            <p className="chakrapara">{this.state.content.function}</p>
+                        </div>
+                        <div>
+                            <h2 className="chakralabels">Related Body Parts</h2>
+                            <p className="chakrapara">{this.state.content.bodyparts}</p>
+                        </div>
+                        <div>
+                            <h2 className="chakralabels">Malfunction</h2>
+                            <p className="chakrapara">{this.state.content.malfunction}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="options">
+                    <Link className="linkchakra" to={`/meditatechakra/${this.props.match.params.id}/WMJ8ZyQuOxQ`}>
                       {/* <Link to={`/meditatechakra/${this.props.match.params.id}/${this.state.content.videoids.15min}`}> */}
                       {/* <Link to={`/meditatechakra/7/WMJ8ZyQuOxQ`}> */}
-                          <div className="optioncard">
-                              <h2>Meditate for 15 minutes</h2>
-                          </div>
-                      </Link>    
-                      <Link className="linkchakra" to={`/meditatechakra/${this.props.match.params.id}/Mrr4dK5Nja0`}>
-                          <div className="optioncard">
-                              <h2>Meditate for 30 minutes</h2>
-                          </div>
-                      </Link>
-  
-                      <Link className="linkchakra" to={`/meditatechakra/${this.props.match.params.id}/vq4TZexuG9Y`}>
-                          <div className="optioncard">
-                              <h2>Meditate for 60 minutes</h2>
-                          </div>
-                      </Link>
-              </div>
-              </div>
-          </div>
-      )
-  }
+                        <div className="optioncard">
+                            <h2>Meditate for 15 minutes</h2>
+                        </div>
+                    </Link>    
+                    <Link className="linkchakra" to={`/meditatechakra/${this.props.match.params.id}/Mrr4dK5Nja0`}>
+                        <div className="optioncard">
+                            <h2>Meditate for 30 minutes</h2>
+                        </div>
+                    </Link>
+
+                    <Link className="linkchakra" to={`/meditatechakra/${this.props.match.params.id}/vq4TZexuG9Y`}>
+                        <div className="optioncard">
+                            <h2>Meditate for 60 minutes</h2>
+                        </div>
+                    </Link>
+            </div>
+            </div>
+        </div>
+    )
+    }
 }
 
 export default Meditate
